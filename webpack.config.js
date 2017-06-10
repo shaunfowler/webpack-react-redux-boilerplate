160ad4be594820b19727e5bd285edaf2ff96d509
Shaun Fowler <Shaun.Fowler@qlik.com>
2017-06-10 15:35:18 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve('dist'),
+		filename: 'index.js'
+	},
+	module: {
+		loaders: [{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.jsx$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+			inject: 'body'
+		})
+	]
+}
