22b5b724bfe71facc80e58dbacc69974b1b64fc2
Shaun Fowler <Shaun.Fowler@qlik.com>
2017-07-31 11:03:31 -0400

    redux store setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from 'react';
+import {connect} from 'react-redux';
+import * as itemActions from '../actions/itemActions';
 
-const Home = () => (
-    <div>
-        <h1>Home</h1>
-    </div>
-)
+class Home extends React.Component {
 
-export default Home
+    constructor(state) {
+        super(state);
+        this.state = {
+            item: {
+                name: ''
+            }
+        }
+
+        this.onItemNameChange = this.onItemNameChange.bind(this);
+        this.onAddItem = this.onAddItem.bind(this);
+    }
+
+    onItemNameChange(event) {
+        const item = this.state.item;
+        item.name = event.target.value;
+        this.setState({item});
+    }
+
+    onAddItem() {
+        this.props.dispatch(itemActions.createItem(this.state.item));
+        this.setState({
+            item: {
+                name: ''
+            }
+        })
+    }
+
+    itemRow(item, index) {
+        return (
+            <li key={index}>{item.name}</li>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+
+                <h2>Items</h2>
+
+                <h3>Add item</h3>
+                <input type="text" onChange={this.onItemNameChange} value={this.state.item.name}/>
+                <button onClick={this.onAddItem}>Add</button>
+
+                <h3>Existing items</h3>
+                {this.props.items.length === 0 ? <div>No items</div> : ''}
+                <ul>
+                    {this.props.items.map(this.itemRow)}
+                </ul>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state, ownProps) {
+    return {items: state.items};
+}
+
+export default connect(mapStateToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
 import App from './components/App';
 
+const store = configureStore();
+
 render((
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'))
